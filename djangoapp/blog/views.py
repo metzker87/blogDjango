bda26ed9b1f151e4d1866996990ac6eedefc20ec
Juliano Metzker <julianometzker87@gmail.com>
2024-01-27 19:16:38 -0300

    implemented Class Based View 554

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,28 +1,53 @@
+from typing import Any
 from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.db.models import Q
 from django.http import Http404
 from django.shortcuts import render
 from blog.models import Page, Post
+from django.views.generic import ListView
 
 PER_PAGE = 9
 
 
-def index(request):
-    posts = Post.objects.get_published()
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/pages/index.html'
+    context_object_name = 'posts'
+    ordering = '-pk',
+    paginate_by = PER_PAGE
+    queryset = Post.objects.get_published()
+    
+    # def get_queryset(self):
+    #     queryset = super().get_queryset()
+    #     queryset = queryset.filter(is_published=True)
+    #     return queryset
 
-    paginator = Paginator(posts, PER_PAGE)
-    page_number = request.GET.get("page")
-    page_obj = paginator.get_page(page_number)
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': page_obj,
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context.update({
             'page_title': 'Home - ',
-        }
-    )
+        })
+
+
+        return context
+
+# def index(request):
+#     posts = Post.objects.get_published()
+
+#     paginator = Paginator(posts, PER_PAGE)
+#     page_number = request.GET.get("page")
+#     page_obj = paginator.get_page(page_number)
+
+#     return render(
+#         request,
+#         'blog/pages/index.html',
+#         {
+#             'page_obj': page_obj,
+#             'page_title': 'Home - ',
+#         }
+#     )
 
 
 def created_by(request, author_pk):
@@ -38,7 +63,6 @@ def created_by(request, author_pk):
     if user.first_name:
         user_full_name = f'{user.first_name} {user.last_name}'
     page_title = 'Posts de ' + user_full_name + ' - '
-
     paginator = Paginator(posts, PER_PAGE)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
